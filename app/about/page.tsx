61945e43a06e796c6dd736fcdb2b953d7ffdb31e
Joe McIntyre <j_h_mcintyre@yahoo.com>
2022-12-26 10:43:32 -0600

    Add h1 styling, about page layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,10 @@
 export default function About () {
   return (
-    <div className='max-w-screen-lg mx-auto p-2'>
-      <h1 className='mb-4'>About Pollster</h1>
-      <p>Content</p>
+    <div className='max-w-screen-lg mx-auto p-2 mt-4'>
+      <h1 className='mb-4 text-center'>About Pollster</h1>
+      <div className='max-w-screen-md mx-auto'>
+        <p>Content</p>
+      </div>
     </div>
   );
 }
